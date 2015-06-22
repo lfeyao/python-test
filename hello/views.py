187b5207a896504468cca529874e13386a7d4897
Dai Yu <lfeyao@gmail.com>
2015-06-21 22:57:52 -0700

    config vars

--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,16 @@
+import os
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
 
+
+
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+    times = int(os.environ.get('TIMES',3))
+    return HttpResponse('Hello! ' * times)
 
 
 def db(request):
